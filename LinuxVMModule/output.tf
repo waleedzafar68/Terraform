6b8c12a02a3c3458ccb2f2fd7d1525bd35012c8a
waleedzafar68 <waleedzafar.68@gmail.com>
2022-09-11 02:34:54 +0500

    changes to IP output error

--- LinuxVMModule/output.tf
+++ LinuxVMModule/output.tf
@@ -4,9 +4,3 @@ output "vmNameOut" {
 output "vmIDOut" {
   value = azurerm_linux_virtual_machine.VTFWPvm.id
 }
-output "domain_name_label" {
-  value = data.azurerm_public_ip.wppubip.domain_name_label
-}
-output "public_ip_address" {
-  value = data.azurerm_public_ip.wppubip.ip_address
-}
--- output.tf
+++ output.tf
@@ -5,10 +5,3 @@ output "vmNameOut" {
 output "vmIDOut" {
   value = module.LinuxVMModule.vmIDOut
 }
-output "domain_name_label" {
-  value = module.LinuxVMModule.domain_name_label
-}
-
-output "public_ip_address" {
-  value = module.LinuxVMModule.public_ip_address
-}
