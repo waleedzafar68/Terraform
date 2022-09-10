3663e49e653a008db71468f7ec8bfbf504514d04
waleedzafar68 <waleedzafar.68@gmail.com>
2022-09-11 01:51:35 +0500

    Created Output.tf for root module

---
+++ output.tf
@@ -0,0 +1,14 @@
+
+output "vmNameOut" {
+  value = module.LinuxVMModule.vmNameOut
+}
+output "vmIDOut" {
+  value = module.LinuxVMModule.vmIDOut
+}
+output "domain_name_label" {
+  value = module.LinuxVMModule.domain_name_label
+}
+
+output "public_ip_address" {
+  value = module.LinuxVMModule.public_ip_address
+}
