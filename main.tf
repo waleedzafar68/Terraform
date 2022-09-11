f049d2a054973a5b086e86fddd7a7be8be1108ea
waleedzafar68 <waleedzafar.68@gmail.com>
2022-09-12 01:42:02 +0500

    Added Rule 443

--- main.tf
+++ main.tf
@@ -77,6 +77,19 @@ resource "azurerm_network_security_rule" "Rule443" {
   resource_group_name         = azurerm_resource_group.lab.name
   network_security_group_name = azurerm_network_security_group.nsg.name
 }
+ resource "azurerm_network_security_rule" "RDP" {
+  name                        = "RDP"
+  priority                    = 1002
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.lab.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
 
 #Attaching Subnet to NSG
 resource "azurerm_subnet_network_security_group_association" "nsgDMZsn" {
