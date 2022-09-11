1fc30825e247488d6fb52e61079eb764b1637b46
waleedzafar68 <waleedzafar.68@gmail.com>
2022-09-12 02:55:35 +0500

    Fixed Output. Human Error

--- LinuxVMModule/output.tf
+++ LinuxVMModule/output.tf
@@ -1,8 +1,8 @@
 output "vmNameOut" {
-  value = azurerm_linux_virtual_machine.DomainControl.name
+  value = azurerm_linux_virtual_machine.VTFWPvm.name
 }
 output "vmIDOut" {
-  value = azurerm_linux_virtual_machine.DomainControl.id
+  value = azurerm_linux_virtual_machine.VTFWPvm.id
 }
 output "pubIPOut"{
   value = azurerm_public_ip.wppubip.ip_address
