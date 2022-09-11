03a6387807e8d7288f1f39aee8723420110d0154
waleedzafar68 <waleedzafar.68@gmail.com>
2022-09-12 01:39:26 +0500

    Created Module for Windows Server

--- LinuxVMModule/output.tf
+++ LinuxVMModule/output.tf
@@ -1,8 +1,8 @@
 output "vmNameOut" {
-  value = azurerm_linux_virtual_machine.VTFWPvm.name
+  value = azurerm_linux_virtual_machine.DomainControl.name
 }
 output "vmIDOut" {
-  value = azurerm_linux_virtual_machine.VTFWPvm.id
+  value = azurerm_linux_virtual_machine.DomainControl.id
 }
 output "pubIPOut"{
   value = azurerm_public_ip.wppubip.ip_address
