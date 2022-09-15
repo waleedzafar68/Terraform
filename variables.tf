4f65738508cb8634587a917d3955483fabcd57b6
waleedzafar68 <waleedzafar.68@gmail.com>
2022-09-15 06:48:42 +0500

    "Kali Deployment added"

--- variables.tf
+++ variables.tf
@@ -8,3 +8,26 @@ variable "adminPass" {
   description = "It is admin password. Set it always using -var 'adminPass = <Password>'"
   sensitive = true
 }
+variable "LinuxVMCount" {
+  description = "Number of machines to be Provisioned (Linux)"
+  type = number
+}
+variable "LinuxMachineNames" {
+  description = "Name of the Linux Machines"
+  type = list(string)
+}
+variable "pubIPNamesLinux" {
+  description = "Name of the associated Public IPs (Linux VMs)"
+}
+variable "domainLabelsLinux" {
+  description = "Name of the domain Names of Linux VMs"
+  type = list(string)
+}
+variable "subNetIDs" {
+  description = "IDs of the subnets these Linux machines will be provisioned in"
+  type = list(string)
+}
+variable "nicConfigNames" {
+  description = "Name of the NIC configs (Linux VMs)"
+  type = list(string)
+}
